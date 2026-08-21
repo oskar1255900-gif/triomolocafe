905265e9726d2c061fea49b05b9a67f9cd791349
oskar1255900-gif <oskar1255900@gmail.com>
2026-08-21 14:34:41 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-# Dodanie katalogu głównego do ścieżki Python
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from backend.server import app
